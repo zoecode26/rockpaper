4ff596f8f9809fc2c8d1c8279f09b5b71d29d74a
zoethomas <zoethomas1540@gmail.com>
2020-10-08 14:05:06 +0100

    Compares choices and decides winner

--- rockpaper.rb
+++ rockpaper.rb
@@ -8,3 +8,17 @@ actions = {
   "lizard"=>["spock", "paper"],
   "spock"=>["scissors", "rock"]
 }
+
+comp_choice = actions.keys.sample
+puts comp_choice
+
+user_targets = actions[user_choice]
+comp_targets = actions[comp_choice]
+
+if user_choice == comp_choice
+  puts "Its a draw"
+elsif user_targets.include?(comp_choice)
+  puts "User wins"
+else
+  puts "Comp wins"
+end
